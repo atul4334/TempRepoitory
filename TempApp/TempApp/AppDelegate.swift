fd3f384cb9624741d00af6a610fa41773af9d907
Atul Ghorpade <atulghorpade7@yahoo.com>
2018-08-18 19:14:25 +0530

    Added log from develop branch

--- TempApp/TempApp/AppDelegate.swift
+++ TempApp/TempApp/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         NSLog("from master123");
+        NSLog("New from develop branch");
         return true
     }
 
